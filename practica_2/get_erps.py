7baf47ad80e71a7a87551c2b2e540f8c528db968
joctavio287 <joctavio287@gmail.com>
2024-10-10 11:49:39 -0300

    erp analysis

--- practica_2/get_erps.py
+++ practica_2/get_erps.py
@@ -1,12 +1,12 @@
 # Standard libraries 
-import numpy as np, matplotlib.pyplot as plt
+import numpy as np, matplotlib.pyplot as plt, os
 
 # Modules
 from practica_2.auxiliary import dump_pickle, load_pickle
 
-raws_LRP = load_pickle(path='practica_2/input/raws_LRP.pkl')
-raws_ERN = load_pickle(path='practica_2/input/raws_ERN.pkl')
-epochs_LRP = load_pickle(path='practica_2/input/epochs_LRP.pkl')
+# raws_LRP = load_pickle(path='practica_2/input/raws_LRP.pkl')
+# raws_ERN = load_pickle(path='practica_2/input/raws_ERN.pkl')
+# epochs_LRP = load_pickle(path='practica_2/input/epochs_LRP.pkl')
 epochs_ERN = load_pickle(path='practica_2/input/epochs_ERN.pkl')
 
 # Subjects 34, 35, 36 have no epochs. The rest have too little responses or non in one category
@@ -37,8 +37,8 @@ avg_incorrect = np.stack(incorrect)
 # ch_interest = raws_ERN[0].ch_names.index('FCz') # C3 es 4 C4 es 22 FCz es 20
 ch_interest = 20
 
-y1 = np.mean(avg_correct,0)[ch_interest,:]
-y2 = np.mean(avg_incorrect,0)[ch_interest,:]
+y1 = np.mean(avg_correct, 0)[ch_interest,:]
+y2 = np.mean(avg_incorrect, 0)[ch_interest,:]
 y1L = y1-(np.std(avg_correct,axis=0)/np.sqrt(avg_correct.shape[0])-1)[ch_interest,:]
 y1H = y1+(np.std(avg_correct,axis=0)/np.sqrt(avg_correct.shape[0])-1)[ch_interest,:]
 y2L = y2-(np.std(avg_incorrect,axis=0)/np.sqrt(avg_incorrect.shape[0])-1)[ch_interest,:]
@@ -56,6 +56,8 @@ plt.yticks([-10,-5,0,5,10,15])
 plt.grid(visible=True)
 plt.legend(title='FCz')
 plt.show(block=False)
+plt.savefig(os.path.join('practica_2/output/', 'corrects_incorrects.png'), dpi=800)
+
 
 y = np.mean((avg_incorrect-avg_correct), axis=0)[ch_interest,:]
 # deviation = np.sqrt((np.std(avg_incorrect,axis=0)/np.sqrt(avg_incorrect.shape[0]))**2+ (np.std(avg_incorrect,axis=0)/np.sqrt(avg_incorrect.shape[0]))**2)
@@ -74,4 +76,40 @@ plt.yticks([-18,-12,-6,0,6,12])
 plt.grid(visible=True)
 plt.legend(title='FCz')
 plt.show(block=False)
-plt.savefig(os.path.join('practica_1/output_experimento_1/imagenes', 'traintest.png'), dpi=600)
+plt.savefig(os.path.join('practica_2/output/', 'ERN_ERP.png'), dpi=800)
+
+freqs = np.array([4,6,8,12,18])
+
+power_car, itc = epochs['Correct'].compute_tfr(
+                                            method="morlet",
+                                            freqs=freqs,
+                                            n_cycles=2,
+                                            average=True,
+                                            return_itc=True,
+                                            decim=3,
+                                            )
+# dividing by the mean of baseline values and taking the log (
+plt.figure()
+data = power_car.get_data()[13, :, :]
+
+inicial = power_car.times.tolist().index(-0.19140625)
+final = power_car.times.tolist().index(0.0078125)
+# data /= data[:, inicial:final].mean()
+data /= data[:, inicial:final].mean()
+# data -= data[:, :52].mean()
+im = plt.pcolormesh(data[:, :], cmap='coolwarm', vmin=-data.max(), vmax=data.max())
+# im = plt.pcolormesh(itc.get_data()[13, :, :])
+plt.colorbar(im
+             )
+            #   boundaries = np.linspace(-.8,.8, 50))
+plt.show(block=False)
+
+
+power_car.plot('Pz', 
+                baseline=(-.2, 0), 
+                mode="logratio", 
+                # vlim=(-data.max(),data.max()), 
+               title='Cara', 
+               cmap='coolwarm')
+‘mean’ | ‘ratio’ | ‘logratio’ | ‘percent’ | ‘zscore’ | ‘zlogratio’
+power_car.get_data().shape # channels, freqs, times
